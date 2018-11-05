01a3e089048674f1b1075bf3b2ddb054897f1ac6
yagami0324 <yagami.t.0324@gmail.com>
2018-11-05 20:50:26 +0900

    ブランチの移動テスト

--- Controller.php
+++ Controller.php
@@ -1,43 +1,45 @@
 <?php
 require "Model.php";
 
-$pokermodel = new PokerModel;
+test
+
+$poker_model = new PokerModel;
 
 echo '【初期手札】'.PHP_EOL;
-$pokermodel->makeHand();
-$pokermodel->viewHand();
-$pokermodel->checkPokerHand();
-$pokermodel->viewPokerHandNameJa();
+$poker_model->makeHand();
+$poker_model->viewHand();
+$poker_model->checkPokerHand();
+$poker_model->viewPokerHandNameJa();
 echo PHP_EOL;
 
 echo '【マリガン後】'.PHP_EOL;
-$pokermodel->changeHand(0b10101);
-$pokermodel->viewHand();
-$pokermodel->checkPokerHand();
-$pokermodel->viewPokerHandNameJa();
+$poker_model->changeHand(0b10101);
+$poker_model->viewHand();
+$poker_model->checkPokerHand();
+$poker_model->viewPokerHandNameJa();
 echo PHP_EOL;
 
 echo '【デバッグ用手札】'.PHP_EOL;
-$pokermodel->setDebugHand( [1,10,11,53,13] );
-$pokermodel->viewHand();
-$pokermodel->checkPokerHand();
-$pokermodel->viewPokerHandNameJa();
+$poker_model->setDebugHand( [14,10,11,1,13] );
+$poker_model->viewHand();
+$poker_model->checkPokerHand();
+$poker_model->viewPokerHandNameJa();
 echo PHP_EOL;
 
 //確認用
-$a_num_joker = $pokermodel->setNumJoker();
-$b_array_sameRank = $pokermodel->setArraySameRank();
-$c_flg_flush = $pokermodel->setFlgFlush();
-$d_flg_straight = $pokermodel->setFlgStraight();
-$e_royal = $pokermodel->is_RoyalStraightFlush();
-$f_five = $pokermodel->is_FiveCard();
-$g_straightflush = $pokermodel->is_StraightFlush();
-$h_four = $pokermodel->is_FourOfAKind();
-$i_fullhouse = $pokermodel->is_FullHouse();
-$j_flush = $pokermodel->is_Flush();
-$k_straight = $pokermodel->is_Straight();
-$l_three = $pokermodel->is_ThreeOfAKind();
-$m_two = $pokermodel->is_TwoPair();
-$n_one = $pokermodel->is_OnePair();
+$a_num_joker = $poker_model->setNumJoker();
+$b_array_sameRank = $poker_model->setArraySameRank();
+$c_flg_flush = $poker_model->setFlgFlush();
+$d_flg_straight = $poker_model->setFlgStraight();
+$e_royal = $poker_model->isRoyalStraightFlush();
+$f_five = $poker_model->isFiveCard();
+$g_straightflush = $poker_model->isStraightFlush();
+$h_four = $poker_model->isFourOfAKind();
+$i_fullhouse = $poker_model->isFullHouse();
+$j_flush = $poker_model->isFlush();
+$k_straight = $poker_model->isStraight();
+$l_three = $poker_model->isThreeOfAKind();
+$m_two = $poker_model->isTwoPair();
+$n_one = $poker_model->isOnePair();
 
 echo 'end';
