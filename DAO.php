ebca5cafd7156bb4e96b4236ea92962559cabac1
yagami0324 <yagami.t.0324@gmail.com>
2018-11-27 21:28:30 +0900

    データベース接続用クラスを作成開始
    ■コンストラクタでデータベース接続
    ■insertPokerEntryメソッドの追加

--- DAO.php
+++ DAO.php
@@ -1,31 +1,60 @@
 <?php
 
-// 1. 変数の宣言
-$dsn  = 'mysql:dbname=GrandBlueCasino;host=localhost';
-$user = "gbf_master";
-$pass = "GraPass";
-$test = "PDO_Result:";
+class Dao
+{
 
-// 2. try&catchを使いつつPDOでデータベース接続
-try{    
-    $pdo = new PDO($dsn,$user,$pass);
-} catch (PDOException $e) {
-    exit('データベースに接続できませんでした。' . $e->getMessage());
-} 
+    private $pdo = null;
+
+    function __construct()
+    {
+
+        //接続先情報
+        $dsn  = 'mysql:dbname=GrandBlueCasino;host=localhost';
+        $user = "gbf_master";
+        $pass = "GraPass";
+        $test = "PDO_Result:";
+
+        //データベース接続
+        try{    
+            $this->pdo = new PDO($dsn,$user,$pass);
+        } catch (PDOException $e) {
+            exit('データベースに接続できませんでした。' . $e->getMessage());
+        } 
+
+    }
 
 
-// 3. SQL文を実行、表示
-$stmt = $pdo->query('SQL文');
+    public function insertPokerEntry(int $user_id, datetime $start_time)
+    {
 
-if (!$stmt){
-    $info = $pdo->errorInfo();
+        $stmt = null;
 
-    exit($info[2]);
+        $stmt = $this->pdo->prepare("
+            INSERT INTO poker_entry (
+                user_id,
+                start_time,
+                finish_time
+            )
+            VALUES (  
+                :user_id,
+                :start_time,
+                ''
+            )
+        ");
+
+        if (!$stmt){
+            $info = $pdo->errorInfo();
+            exit($info[2]);
+        }
+
+        $stmt->execute( 
+                [ 
+                    ":user_id" => $user_id ,
+                    ":start_time" => $start_time->format("Y-m-d H:i:s")
+                ]
+            );
+
+    } 
+        
+
 }
-    while($data= $stmt->fetch(PDO::FETCH_ASSOC)){
-        echo $testName;
-        echo '<p>'. $data['col1'] ."</p>\n";
-}
-
-    // 4. データベースから切断
-    $pdo = null;
